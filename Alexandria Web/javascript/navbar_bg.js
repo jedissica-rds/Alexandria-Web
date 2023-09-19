e53d88f4177fc2958459ca249e2464b5b293ed16
Jessica Souza <jessicasouza.dev@gmail.com>
2023-09-19 16:23:11 -0400

    first commit

---
+++ Alexandria Web/javascript/navbar_bg.js
@@ -0,0 +1,8 @@
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() <= 10) {
+     $('.navbar').css('background', 'transparent');
+    } else {
+      $('.navbar').css('background', '##856DDA');
+}
+});
